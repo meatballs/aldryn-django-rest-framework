b7986ff1997bf0119cb1b9e4cac6cc743282b125
Owen Campbell <owen.campbell@tanti.org.uk>
2016-09-27 13:38:50 +0100

    Wrap settings in condition for addon enabled

--- aldryn_config.py
+++ aldryn_config.py
@@ -14,18 +14,14 @@ class Form(forms.BaseForm):
     def to_settings(self, data, settings):
         enable_rest_framework = data['enable_rest_framework']
 
-        from aldryn_addons.utils import djsenv
-        env = partial(djsenv, settings=settings)
-        env_stage = '' if env('STAGE') is None else str(env('STAGE'))
-        development_mode = True if env_stage in ['test', 'local'] else False
-
-        settings['INSTALLED_APPS'].extend([
-            'rest_framework',
-        ])
-        settings['ADDON_URLS'].append('aldryn_django_rest_framework.urls')
-        settings['REST_FRAMEWORK'] = {
-            'DEFAULT_PERMISSION_CLASSES': [
-                'rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly'
-            ]
-        }
+        if enable_rest_framework:
+            settings['INSTALLED_APPS'].extend([
+                'rest_framework',
+            ])
+            settings['ADDON_URLS'].append('aldryn_django_rest_framework.urls')
+            settings['REST_FRAMEWORK'] = {
+                'DEFAULT_PERMISSION_CLASSES': [
+                    'rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly'
+                ]
+            }
         return settings
